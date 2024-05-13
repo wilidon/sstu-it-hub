ad1fa00bfa49a5648d97fef92241fcb3b7d69e8c
ildar <wilidon@bk.ru>
2024-05-13 22:39:55 +0400

    added id

--- src/main/java/ru/sstu/studentprofile/domain/service/request/dto/RequestOut.java
+++ src/main/java/ru/sstu/studentprofile/domain/service/request/dto/RequestOut.java
@@ -1,6 +1,7 @@
 package ru.sstu.studentprofile.domain.service.request.dto;
 
 public record RequestOut(
+        long id,
         SenderOut sender,
         long recipient_id,
         long project_id,
